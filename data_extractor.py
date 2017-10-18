fcfa9668083ee092c4887e13f57fee9f1756fa37
Omarkhursheed <omarkhu@gmail.com>
2017-10-19 02:55:45 +0530

    pulled all data from a single stock exchange

--- data_extractor.py
+++ data_extractor.py
@@ -1,6 +1,17 @@
 import quandl
 import pandas as pd
-#api_key = yvR18HYhyiyR51fL9UM5
-data  = quandl.bulkdownload("ZEA?api_key=yvR18HYhyiyR51fL9UM5")
+import json
+import string
+quandl.ApiConfig.api_key = 'yvR18HYhyiyR51fL9UM5'
+#data  = quandl.get("NSE/BSLGOLDETF")
+#data = quandl.get("https://www.quandl.com/api/v3/databases/LSE/codes.json")
+#print(data)
+codes_list_file = pd.read_csv("LSE-datasets-codes.csv")
 
-print(data)
+for exchange in codes_list_file['LSE/DAILY_TRADES']:
+	df = quandl.get(exchange)
+	exchange1 = exchange.replace('/','-',1)
+	file_path =  'data_files/' + exchange1 + '.csv' 
+	
+	df.to_csv(file_path)
+	print("all")
