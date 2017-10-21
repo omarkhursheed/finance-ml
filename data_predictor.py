8a8e0a594a31da6927f18a8440f3c3b3c0397ba4
Omarkhursheed <omarkhu@gmail.com>
2017-10-21 22:34:30 +0530

    added ARIMA implementation

--- data_predictor.py
+++ data_predictor.py
@@ -1,66 +1,76 @@
 from __future__ import print_function
 
-import datetime
-import pandas as pd 
-import numpy as np
-import sklearn
+from pandas import Series
+from pandas import DataFrame
+from statsmodels.tsa.arima_model import ARIMA
+from matplotlib import pyplot
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
-from pandas_datareader import DataReader
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-#from sklearn.lda import LDA
-from sklearn.metrics import confusion_matrix
-#from sklearn.qda import QDA
-from sklearn.svm import LinearSVC, SVC
+# create a differenced series
+def difference(dataset, interval=1):
+	diff = list()
+	for i in range(interval, len(dataset)):
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return diff
 
-def create_serice(symbol, start, end, lags = 5):
-	ts = DataReader(
-		symbol, "yahoo",start-datetime.timedelta(days = 365),
-		end)
-
-	tslag = pd.DataFrame(index = ts.index)
-	tslag["Today"] = ts["Adj Close"]
-	tslag["Volume"] = ts["Volume"]
-	tsret["Today"] = tslag["Today"].pct_change()*100.0
-
-	for i,x in enumerate(tsret["Today"]):
-		if(abs(x) < 0.0001):
-			tsret["Today"][i] = 0.0001
-
-	for i in range(0, lags):
-		tsret["Lag%s" % str(i+1)] = \
-		tslag["Lag%s" % str(i+1)].pct_change()*100.0
-
-	tsret["Direction"] = np.sign(tsret["Today"])
-	tsret = tsret[tsret.index >= start_dae]
-
-	return tsret
+def inverse_difference(history, yhat, interval=1):
+	return yhat + history[-interval]
 
 
-if __name__ == "__main__":
-	snpret = create_serice("^GSPC",datetime.datetime(2001,1,10),datetime.datetime(2005,12,31), lags=5)
-	X = snpret[["Lag1","Lag2"]]
-	y = snpret["Direction"]
+series = Series.from_csv('LSE-ABDP-dataset.csv')
+X = series.values
+X = X.astype('float32')
+train_size = int(len(X) * 0.50)
+train, test = X[0:train_size], X[train_size:]
+# walk-forward validation
+history = [x for x in train]
+predictions = list()
+#bias = 165.904728
+months_in_year = 12
+diff = difference(history, months_in_year)
+# predict
+model = ARIMA(diff, order=(2,0,1))
+	
+for i in range(len(test)):
+	model_fit = model.fit(trend='nc', disp=0)
+	yhat = model_fit.forecast()[0]
+	yhat = inverse_difference(history, yhat, months_in_year)
+	predictions.append(yhat)
+	obs = test[i]
+	history.append(obs)
+	print('>Predicted=%.3f, Expected=%3.f' % (yhat, obs))
 
-	start_test = datetime.datetime(2005, 1,1)
-	X_train = X[X.index < start_test]
-	X_test = X[X.index >= start_test]
-	y_train = y[y.index < start_test]
-	y_test = y[y.index() >= start_test]
-
-	print("Hit rates")
-	models = [("LR, LogisticRegression()"),
-	("LSV", LinearSVC()),
-	("RSVM", SVC(
-	C=1000000.0, cache_size=200, class_weight=None,
-	coef0=0.0, degree=3, gamma=0.0001,
-	max_iter=-1, probability=False, random_state=None,
-	shrinking=True, tol=0.001, verbose=False)
-	),
-	]
-
-	for m in models:
-		m[1].fit(X_train, y_train)
-		pred = m[1].predit(X_test)
-
-		print("%s:\n0.3f"%(m[0],m[1].score(X_test, y_test)))
+validation_series = Series.from_csv('LSE-ABDP-validation.csv')
+Y = validation_series.values
+Y = Y.astype('float32')
+validation_predictions = list()
+history_validation = [x for x in validation_series]
+error_list = []
+for i in range(len(validation_series)):
+	model_fit = model.fit(trend='nc', disp=0)
+	yhat = model_fit.forecast()[0]
+	yhat = inverse_difference(history_validation, yhat, months_in_year)
+	validation_predictions.append(yhat)
+	obs = validation_series[i]
+	history_validation.append(obs)
+	print('>Predicted=%.3f, Expected=%3.f'  % (yhat, obs))
+	error_list.append(abs((yhat - obs)*100/obs))
+print ("Error in validation data set")
+print(sum(error_list)/float(len(error_list)))
+# report performance
+#mse = mean_squared_error(test, predictions)
+#rmse = sqrt(mse)
+#print('RMSE: %.3f' % rmse)
+# errors
+#residuals = [test[i]-predictions[i] for i in range(len(test))]
+#residuals = DataFrame(residuals)
+#print(residuals.describe())
+# plot
+#pyplot.figure()
+#pyplot.subplot(211)
+#residuals.hist(ax=pyplot.gca())
+#pyplot.subplot(212)
+#residuals.plot(kind='kde', ax=pyplot.gca())
+#pyplot.show()
