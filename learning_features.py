25d67c89737a9520a62845b9b77e3e89e068d016
Omarkhursheed <omarkhu@gmail.com>
2017-11-11 12:55:39 +0530

    add

--- learning_features.py
+++ learning_features.py
@@ -33,7 +33,7 @@ df.dropna(inplace=True)
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
-clf = LinearRegression()
+clf = SVR()
 clf.fit(X_train, y_train)
 confidence = clf.score(X_test, y_test)
 print(confidence)
