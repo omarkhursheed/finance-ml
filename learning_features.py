043d06927b31d08488cccceb968c1c47ee401793
Danish Saeed <danishsaeed2@gmail.com>
2017-11-30 13:06:54 +0530

    update

--- learning_features.py
+++ learning_features.py
@@ -20,6 +20,28 @@ os.chdir(dname)
 
 warnings.filterwarnings("ignore")
 
+'''
+import quandl
+import pandas as pd
+import json
+import string
+import os, os.path
+quandl.ApiConfig.api_key = 'yvR18HYhyiyR51fL9UM5'
+dname = os.path.dirname(os.path.abspath(__file__))
+os.chdir(dname)
+
+codes_list_file = pd.read_csv("WIKI-datasets-codes.csv",names=["Code","Name"])
+
+for exchange in codes_list_file['Code']:
+	df = quandl.get(exchange)
+	exchange1 = exchange.replace('/','-',1)
+	file_path =  'data_files/' + exchange1 + '.csv' 
+	
+	df.to_csv(file_path)
+	print(exchange+" - pulled")
+
+'''
+
 df = pd.read_csv('data_files/WIKI-AAPL.csv')
 df['Date'] = pd.to_datetime(df['Date'])
 df = df.set_index('Date')
